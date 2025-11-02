9eca621b29e9e65a0ed170c84e221ce92c9edc0c
NCH-01 <proficientwitch@gmail.com>
2025-11-03 05:25:05 +0700

    Add autoAssign button for Editor

---
+++ Assets/00_Core/AutoAssign/AutoAssignBase.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+// define Attribute to mark things to assignment
+public class AutoAssign : PropertyAttribute { }
+
+public abstract class AutoAssignBase : MonoBehaviour
+{
+    
+}
--- Assets/01_Characters/Player/PlayerCtrl.cs
+++ Assets/01_Characters/Player/PlayerCtrl.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
-public class PlayerCtrl : MonoBehaviour
+public class PlayerCtrl : AutoAssignBase
 {
-
+    [SerializeField][AutoAssign] protected Rigidbody2D rb;
+    [SerializeField][AutoAssign] protected BoxCollider2D boxCollider;
+    [SerializeField][AutoAssign] protected Animator animator;
 }
